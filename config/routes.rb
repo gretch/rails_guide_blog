cdf0460aa29005968557d7facd1fe5f8a7124570
Gretch Gapol <ggapol@nerubia.com>
2014-07-03 12:01:38 +0800

    Use articles

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ get "welcome/index"
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+   root 'articles#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
